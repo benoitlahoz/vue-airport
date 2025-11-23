ca24e92b028762d26df8dd15415ed8c165c43e56
Benoît LAHOZ <lahoz.ben@gmail.com>
2025-11-23 16:00:11 +0100

    transformValue plugin

--- packages/plugins-base/src/index.ts
+++ packages/plugins-base/src/index.ts
@@ -1,3 +1,8 @@
 export { createActiveItemPlugin } from './activeItem';
 export { createHistoryPlugin, type HistoryOptions, type HistoryEntry } from './history';
 export { createDebouncePlugin, type DebounceOptions } from './debounce';
+export {
+  createTransformValuePlugin,
+  type TransformConfig,
+  type Transforms,
+} from './transformValue';
